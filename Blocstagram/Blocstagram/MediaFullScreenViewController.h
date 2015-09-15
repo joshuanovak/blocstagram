fdc21722fc62ae054f34b13f48684f9f47ee5e48
Joshua Novak <josh19novak@gmail.com>
2015-09-15 09:33:19 -0500

    Checkpoint 36 compleation

---
+++ Blocstagram/Blocstagram/MediaFullScreenViewController.h
@@ -0,0 +1,13 @@
+//
+//  MediaFullScreenViewController.h
+//  Blocstagram
+//
+//  Created by Joshua on 9/15/15.
+//  Copyright (c) 2015 Joshua Novak. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MediaFullScreenViewController : UIViewController
+
+@end
