4e20e76b310894cf2e71c13dcedb68ee1c3e68b2
Joshua Novak <josh19novak@gmail.com>
2015-12-28 09:59:54 -0600

    Checkpoint 43 Compleation

---
+++ Blocstagram/Blocstagram/PostToInstagramViewController.h
@@ -0,0 +1,15 @@
+//
+//  PostToInstagramViewController.h
+//  Blocstagram
+//
+//  Created by Joshua on 12/28/15.
+//  Copyright © 2015 Joshua Novak. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PostToInstagramViewController : UIViewController
+
+- (instancetype) initWithImage:(UIImage *)sourceImage;
+
+@end
