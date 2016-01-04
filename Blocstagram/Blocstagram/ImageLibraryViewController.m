c01ae19ba6ee96e653d6f1f07ecb481278cfdb4a
Joshua Novak <josh19novak@gmail.com>
2016-01-04 10:06:46 -0600

    Checkpoint 44 (with errors)

--- Blocstagram/Blocstagram/ImageLibraryViewController.m
+++ Blocstagram/Blocstagram/ImageLibraryViewController.m
@@ -71,6 +71,7 @@
     options.sortDescriptors = @[[NSSortDescriptor sortDescriptorWithKey:@"creationDate" ascending:NO]];
     
     self.result = [PHAsset fetchAssetsWithMediaType:PHAssetMediaTypeImage options:options];
+    [self.collectionView reloadData];
 }
 
 - (void) viewWillAppear:(BOOL)animated {
@@ -81,7 +82,6 @@
             if ([PHPhotoLibrary authorizationStatus] == PHAuthorizationStatusAuthorized) {
                 dispatch_async(dispatch_get_main_queue(), ^{
                     [self loadAssets];
-                    [self.collectionView reloadData];
                 });
             }
         }];
