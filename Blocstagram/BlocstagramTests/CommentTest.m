2a1f65fcb65ce101504956de0fa1a442984c3cce
Joshua Novak <josh19novak@gmail.com>
2016-01-23 15:30:48 -0600

    Assignment 45 1/23 updates

---
+++ Blocstagram/BlocstagramTests/CommentTest.m
@@ -0,0 +1,67 @@
+//
+//  CommentTest.m
+//  Blocstagram
+//
+//  Created by Joshua on 1/23/16.
+//  Copyright © 2016 Joshua Novak. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+#import "Comment.h"
+#import "ComposeCommentView.h"
+
+@interface CommentTest : XCTestCase
+
+@end
+
+@implementation CommentTest
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+    XCTAssert(YES, @"pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+- (void)testInitalization
+{
+    NSDictionary *sourceDIctionary = @{@"id": @"83018408",
+                                       @"text": @"Hey I have an opinion..."};
+    
+    Comment *someComment = [[Comment alloc] initWithDictionary:sourceDIctionary];
+    
+    XCTAssertEqualObjects(someComment.idNumber, sourceDIctionary[@"id"], @"The ID's should be equal");
+    XCTAssertEqualObjects(someComment.text, sourceDIctionary[@"text"], "The text should be equal");
+}
+
+- (void)testIsWriting
+{
+    BOOL *isWriting = YES;
+    
+}
+
+
+
+
+
+
+
+
+
+@end
--- Blocstagram/BlocstagramTests/MediaTests.m
+++ Blocstagram/BlocstagramTests/MediaTests.m
@@ -8,18 +8,25 @@
 
 #import <XCTest/XCTest.h>
 #import "Media.h"
-//#import "Media.m"
 #import "User.h"
+#import "Comment.h"
 
 @interface MediaTests : XCTestCase
 //NSDictionary *user
 @end
+
+static NSDictionary *sampleUser = nil;
 
 @implementation MediaTests
 
 - (void)setUp {
     [super setUp];
     // Put setup code here. This method is called before the invocation of each test method in the class.
+    
+    sampleUser = @{@"id": @"6105892",
+                   @"username": @"this1guy",
+                   @"full_name": @"This One Guy",
+                   @"profile_picture": @"http://www.fakepic.com"};
 }
 
 - (void)tearDown {
@@ -29,25 +36,31 @@
 
 - (void)testThatInitializationWorks
 {
-    NSDictionary *user = @{@"username": @"novakjoshua",
-                                     @"full_name": @"Joshua Novak",
-                                     @"id": @"3",
-                                     @"profile_picture": @"http://www.pic.com"};
+    NSDictionary *someComment = @{@"id": @"123456789",
+                                  @"text": @"wow sick pic",
+                                  @"from": sampleUser};
     
     
-    NSDictionary *mediaDictionary = @{@"type": @"image",
-                                      @"user": user,
-                                      @"caption": @"null",
-                                      //@"comments": @"commentsDictionary"
-                                      @"filter": @"waldon"};
+    NSDictionary *mediaDictionary = @{@"id": @"838921",
+                                      @"url": @"www.testmedia.com",
+                                      @"caption": @{@"wow look at this": @"this is neat"},
+                                      @"comments": @{@"data": @[someComment]},
+                                      @"user_has_liked": @"YES",};
     
     Media *testMedia = [[Media alloc] initWithDictionary:mediaDictionary];
     
     
     XCTAssertEqualObjects(testMedia.idNumber, mediaDictionary[@"id"], @"The ID number should be equal");
-    XCTAssertEqualObjects(testMedia.user, mediaDictionary[@"user"], @"The user shoudld be equal");
-    XCTAssertEqualObjects(testMedia.image, [NSURL URLWithString:mediaDictionary[@"image"]], @"The image should be equal");
-    //How do we check the user dictionary or other dictionaries that are declared in the JSON?
+    XCTAssertEqualObjects(testMedia.mediaURL, [NSURL URLWithString:mediaDictionary[@"mediaURL"]], @"The picture URL shoudld be equal")
+    
+    ;
+    XCTAssertEqualObjects(testMedia.caption, mediaDictionary[@"caption"][@"text"], @"The caption should be equal");
+    XCTAssertEqualObjects(@(testMedia.comments.count), @(1), @"There is only one comment");
+    Comment *firstComment = testMedia.comments[0];
+    XCTAssertEqualObjects(firstComment.idNumber, mediaDictionary[@"comments"][@"data"][0][@"id"], @"The ID's should be equal");
+    XCTAssertEqualObjects(firstComment.text, mediaDictionary[@"comments"][@"data"][0][@"text"], @"The text should be equal");
+    User *firstUser = firstComment.from;
+    XCTAssertEqualObjects(firstUser.fullName, mediaDictionary[@"comments"][@"data"][0][@"from"][@"full_name"], @"The user should be equal");
     
 }
 
